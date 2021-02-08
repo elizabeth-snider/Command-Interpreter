03f00c638e4e08f7b45f814c697b498ed170432c
Elizabeth Snider <eys275@cs.utexas.edu>
2021-02-08 17:16:51 -0600

    first commit

---
+++ eval.c
@@ -0,0 +1,303 @@
+/**************************************************************************
+ * C S 429 EEL interpreter
+ * 
+ * eval.c - This file contains the skeleton of functions to be implemented by
+ * you. When completed, it will contain the code used to evaluate an expression
+ * based on its AST.
+ * 
+ * Copyright (c) 2021. S. Chatterjee, X. Shen, T. Byrd. All rights reserved.
+ * May not be used, modified, or copied without permission.
+ **************************************************************************/ 
+
+#include "ci.h"
+
+extern bool is_binop(token_t);
+extern bool is_unop(token_t);
+char *strrev(char *str);
+
+/* infer_type() - set the type of a non-root node based on the types of children
+ * Parameter: A node pointer, possibly NULL.
+ * Return value: None.
+ * Side effect: The type field of the node is updated.
+ * (STUDENT TODO)
+ */
+
+static void infer_type(node_t *nptr) {
+    if(nptr == NULL) return;
+    if(terminate || ignore_input) return;
+    if(nptr->node_type != NT_LEAF){
+        infer_type(nptr->children[0]);
+        infer_type(nptr->children[1]); 
+        if(nptr->children[1] != NULL && nptr->tok != TOK_QUESTION){
+            if(nptr->children[0]->type != nptr->children[1]->type && nptr->tok != TOK_TIMES){
+                handle_error(ERR_TYPE);
+                return;
+            } else if((nptr->tok == TOK_AND || nptr->tok == TOK_OR) && 
+                (nptr->children[0]->type != BOOL_TYPE || nptr->children[1]->type != BOOL_TYPE)){
+                handle_error(ERR_TYPE);
+                return;
+            } else if(nptr->children[0]->type == BOOL_TYPE && (nptr->tok != TOK_AND || nptr->tok != TOK_OR)){
+                handle_error(ERR_TYPE);
+                return;
+            }
+        }
+        nptr->type = nptr->children[0]->type;
+    } else{
+        nptr->type = nptr->type;
+    }
+}
+
+/* infer_root() - set the type of the root node based on the types of children
+ * Parameter: A pointer to a root node, possibly NULL.
+ * Return value: None.
+ * Side effect: The type field of the node is updated. 
+ */
+
+static void infer_root(node_t *nptr) {
+    print_tree(nptr);
+    if (nptr == NULL) return;
+    // check running status
+    if (terminate || ignore_input) return;
+
+    // check for assignment
+    if (nptr->type == ID_TYPE) {
+        infer_type(nptr->children[1]);
+    } else {
+        for (int i = 0; i < 3; ++i) {
+            infer_type(nptr->children[i]);
+        }
+        if (nptr->children[0] == NULL) {
+            logging(LOG_ERROR, "failed to find child node");
+            return;
+        }
+        nptr->type = nptr->children[0]->type;
+    }
+    return;
+}
+
+/* eval_node() - set the value of a non-root node based on the values of children
+ * Parameter: A node pointer, possibly NULL.
+ * Return value: None.
+ * Side effect: The val field of the node is updated.
+ * (STUDENT TODO) 
+ */
+
+static void eval_node(node_t *nptr) {
+    if(nptr == NULL) return;
+    if(terminate || ignore_input) return;
+    if(nptr->node_type != NT_LEAF){
+        eval_node(nptr->children[0]);
+        eval_node(nptr->children[1]);
+        if(nptr->type == INT_TYPE){
+            if(nptr->tok == TOK_PLUS){
+                nptr->val.ival = nptr->children[0]->val.ival + nptr->children[1]->val.ival;
+            } else if(nptr->tok == TOK_DIV){
+                if(nptr->children[1]->val.ival == 0){
+                    handle_error(ERR_EVAL);
+                    return;
+                }
+                nptr->val.ival = nptr->children[0]->val.ival / nptr->children[1]->val.ival;
+            } else if(nptr->tok == TOK_MOD){
+                if(nptr->children[1]->val.ival == 0){
+                    handle_error(ERR_TYPE);
+                    return;
+                }
+                nptr->val.ival = nptr->children[0]->val.ival % nptr->children[1]->val.ival;
+            } else if(nptr->tok == TOK_LT){
+                int one = nptr->children[0]->val.ival;
+                int two = nptr->children[1]->val.ival;
+                if(one < two){
+                    nptr->val.bval = 1;
+                } else{
+                    nptr->val.bval = 0;
+                }
+            } else if(nptr->tok == TOK_GT){
+                int one = nptr->children[0]->val.ival;
+                int two = nptr->children[1]->val.ival;
+                if(one > two){
+                    nptr->val.bval = 1;
+                } else{
+                    nptr->val.bval = 0;
+                }
+            } else if(nptr->tok == TOK_TIMES){
+                if(nptr->children[1]->type == STRING_TYPE){
+                    handle_error(ERR_TYPE);
+                    return;
+                }
+                nptr->val.ival = nptr->children[0]->val.ival * nptr->children[1]->val.ival;
+            } else if(nptr->tok == TOK_BMINUS){
+                nptr->val.ival = nptr->children[0]->val.ival - nptr->children[1]->val.ival;
+
+            } else if(nptr->tok == TOK_EQ){
+                if(nptr->children[0]->val.ival == nptr->children[1]->val.ival){
+                    nptr->val.bval = 1;
+                } else{
+                    nptr->val.bval = 0;
+                }
+            } else if(nptr->tok == TOK_UMINUS){
+                nptr->val.ival = (-1) * (nptr->children[0]->val.ival);
+            } else if(nptr->tok == TOK_NOT){
+                handle_error(ERR_TYPE);
+                return;
+            }
+        } else if(nptr->type == STRING_TYPE){
+            if(nptr->tok == TOK_EQ){
+                int boo = strcmp(nptr->children[0]->val.sval, nptr->children[1]->val.sval);
+                if(boo == 0){
+                    nptr->val.bval = 1;
+                } else{
+                    nptr->val.bval = 0;
+                }
+            } else if(nptr->tok == TOK_UMINUS){
+                nptr->val.sval = strrev(nptr->children[0]->val.sval);
+            } else if(nptr->tok == TOK_PLUS){ 
+                char *str = malloc(strlen(nptr->children[0]->val.sval) + strlen(nptr->children[1]->val.sval) + 1);
+                strcpy(stpcpy(str, nptr->children[0]->val.sval), nptr->children[1]->val.sval);
+                nptr->val.sval = str;
+            } else if(nptr->tok == TOK_TIMES){
+                int count = nptr->children[1]->val.ival;
+                char *str = (char*)malloc(strlen(nptr->children[0]->val.sval) * count + 1);
+                strcpy(str, "");
+                for(int index = 0; index < count; index++){
+                    strcat(str, nptr->children[0]->val.sval);
+                }
+                nptr->val.sval = str;
+            } else if(nptr->tok == TOK_LT){
+                int one = strlen(nptr->children[0]->val.sval);
+                int two = strlen(nptr->children[1]->val.sval);
+                if(one < two){
+                    nptr->val.bval = 1;
+                } else{
+                    nptr->val.bval = 0;
+                }
+            } else if(nptr->tok == TOK_GT){
+                int one = strlen(nptr->children[0]->val.sval);
+                int two = strlen(nptr->children[1]->val.sval);
+                if(one > two){
+                    nptr->val.bval = 1;
+                } else{
+                    nptr->val.bval = 0;
+                }
+            }
+        } else if(nptr->type == BOOL_TYPE){
+            if(nptr->tok == TOK_AND){
+                if(nptr->children[0]->val.bval == true && nptr->children[1]->val.bval == true){
+                    nptr->val.bval = 1;
+                } else{
+                    nptr->val.bval = 0;
+                }
+            } else if(nptr->tok == TOK_OR){
+                if(nptr->children[0]->val.bval == true || nptr->children[1]->val.bval == true){
+                    nptr->val.bval = 1;
+                } else{
+                    nptr->val.bval = 0;
+                }
+            } else if(nptr->tok == TOK_NOT){
+                nptr->val.bval = !(nptr->children[0]->val.bval);
+            } else if(nptr->tok == TOK_QUESTION){
+                if(nptr->children[0]->val.bval == 1){
+                    if(nptr->children[1]->type == INT_TYPE){
+                        if(nptr->children[2]->type == BOOL_TYPE){
+                            handle_error(ERR_TYPE);
+                            return;
+                        }
+                        nptr->val.ival = nptr->children[1]->val.ival;
+                    } else{
+                        nptr->val.bval = nptr->children[1]->val.bval;
+                    }
+                } else {
+                    if(nptr->children[1]->type == INT_TYPE){
+                        nptr->val.ival = nptr->children[2]->val.ival;
+                    } else{
+                        //nptr->val.bval = nptr->children[2]->val.bval;
+                        nptr->val.bval = 0;
+                    }
+                }
+            }
+        }
+    } else{
+        if(nptr->type == BOOL_TYPE){
+            if(nptr->tok == TOK_TRUE){
+                nptr->val.bval = 1;
+            } else if(nptr->tok == TOK_FALSE){
+                nptr->val.bval = 0;
+            }
+        }
+        nptr->val = nptr->val;
+    }
+}
+
+/* eval_root() - set the value of the root node based on the values of children 
+ * Parameter: A pointer to a root node, possibly NULL.
+ * Return value: None.
+ * Side effect: The val dield of the node is updated. 
+ */
+
+void eval_root(node_t *nptr) {
+    if (nptr == NULL) return;
+    // check running status
+    if (terminate || ignore_input) return;
+
+    // check for assignment
+    if (nptr->type == ID_TYPE) {
+        eval_node(nptr->children[1]);
+        if (terminate || ignore_input) return;
+        
+        if (nptr->children[0] == NULL) {
+            logging(LOG_ERROR, "failed to find child node");
+            return;
+        }
+        put(nptr->children[0]->val.sval, nptr->children[1]);
+        return;
+    }
+
+    for (int i = 0; i < 2; ++i) {
+        eval_node(nptr->children[i]);
+    }
+    if (terminate || ignore_input) return;
+    
+    if (nptr->type == STRING_TYPE) {
+        (nptr->val).sval = (char *) malloc(strlen(nptr->children[0]->val.sval) + 1);
+        if (! nptr->val.sval) {
+            logging(LOG_FATAL, "failed to allocate string");
+            return;
+        }
+        strcpy(nptr->val.sval, nptr->children[0]->val.sval);
+    } else {
+        nptr->val.ival = nptr->children[0]->val.ival;
+    }
+    return;
+}
+
+/* infer_and_eval() - wrapper for calling infer() and eval() 
+ * Parameter: A pointer to a root node.
+ * Return value: none.
+ * Side effect: The type and val fields of the node are updated. 
+ */
+
+void infer_and_eval(node_t *nptr) {
+    infer_root(nptr);
+    eval_root(nptr);
+    return;
+}
+
+/* strrev() - helper function to reverse a given string 
+ * Parameter: The string to reverse.
+ * Return value: The reversed string. The input string is not modified.
+ * (STUDENT TODO)
+ */
+
+char *strrev(char *str) {
+    char *start = str;
+    char *end = start + strlen(str) - 1;
+    char temp;
+
+    while(end > start){
+        temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+    return str;
+}
---
+++ parse.c
@@ -0,0 +1,265 @@
+/**************************************************************************
+ * C S 429 EEL interpreter
+ * 
+ * parse.c - This file contains the skeleton of functions to be implemented by
+ * you. When completed, it will contain the code used to parse an expression and
+ * create an AST.
+ * 
+ * Copyright (c) 2021. S. Chatterjee, X. Shen, T. Byrd. All rights reserved.
+ * May not be used, modified, or copied without permission.
+ **************************************************************************/ 
+
+#include "ci.h"
+
+/* Explained in ci.h */
+extern lptr_t this_token, next_token;
+extern void init_lexer(void);
+extern void advance_lexer(void);
+
+/* Valid format specifers */
+static const char *VALID_FMTS = "dxXbB";
+
+/* The only reserved identifiers are "true" and "false" */
+static const int NUM_RESERVED_IDS = 2;
+static const struct {
+    char *id;
+    token_t t;
+} reserved_ids[] = {
+    {"true", TOK_TRUE},
+    {"false", TOK_FALSE}
+};
+
+/* is_binop() - return true if a token represents a binary operator
+ * Parameter: Any token
+ * Return value: true if t is a binary operator, false otherwise */
+bool is_binop(token_t t) {
+    return t >= TOK_PLUS && t <= TOK_EQ;
+}
+
+/* is_unop() - return true if a token represents a unary operator
+ * Parameter: Any token
+ * Return value: true if t is a unary operator, false otherwise */
+bool is_unop(token_t t) {
+    return t >= TOK_UMINUS && t <= TOK_NOT;
+}
+
+/* id_is_fmt_spec() - return true if a string is a format specifier
+ * Parameter: Any string
+ * Return value: true if s is format specifier, false otherwise */
+bool id_is_fmt_spec(char *s) {
+    return strlen(s) == 1 && strspn(s, VALID_FMTS) == 1;
+}
+
+/* check_reserved_ids() - check if a given string is a reserved identifier
+ * Parameter: Any string
+ * Return value: true if s is a reserved identifier, false otherwise */
+static token_t check_reserved_ids(char *s) {
+    for (int i = 0; i < NUM_RESERVED_IDS; i++)
+        if (strcmp(reserved_ids[i].id, s) == 0) return reserved_ids[i].t;
+    return TOK_INVALID;
+}
+
+/* build_leaf() - create a leaf node based on this_token and / or next_token
+ * Parameter: none
+ * Return value: pointer to a leaf node
+ * (STUDENT TODO) */
+static node_t *build_leaf(void) {
+    node_t *leaf = calloc(1, sizeof(node_t));
+    leaf->node_type = NT_LEAF;
+    char *rep = this_token->repr;
+
+    if(this_token->ttype == TOK_NUM){
+        leaf->val.ival = atoi(rep);
+        leaf->type = INT_TYPE;
+        leaf->tok = TOK_NUM;
+    } else if(this_token->ttype == TOK_STR){
+        char *str = malloc(strlen(this_token->repr));
+        strcpy(str, this_token->repr);
+        leaf->val.sval = str;
+        leaf->type = STRING_TYPE;
+        leaf->tok = TOK_STR;
+    } else if(this_token->ttype == TOK_FMT_SPEC){
+        char a = this_token->repr[0];
+        leaf->type = FMT_TYPE;
+        leaf->tok = TOK_FMT_SPEC;
+        leaf->val.fval = a;
+    } else if(this_token->ttype == TOK_TRUE || this_token->ttype == TOK_FALSE){
+        bool boo = this_token->repr;
+        leaf->type = BOOL_TYPE;
+        leaf->tok = this_token->ttype;
+        leaf->val.bval = boo;
+    }
+    return leaf;
+}
+
+/* build_exp() - parse an expression based on this_token and / or next_token
+ * Make calls to build_leaf() or build_exp() if necessary. 
+ * Parameter: none
+ * Return value: pointer to an internal node
+ * (STUDENT TODO */
+static node_t *build_exp(void) {
+    // check running status
+    if (terminate || ignore_input) return NULL;
+    
+    token_t t;
+
+    // The case of a leaf node is handled for you
+    if (this_token->ttype == TOK_NUM)
+        return build_leaf();
+    if (this_token->ttype == TOK_STR)
+        return build_leaf();
+    // handle the reserved identifiers, namely true and false
+    if (this_token->ttype == TOK_ID) {
+        if ((t = check_reserved_ids(this_token->repr)) != TOK_INVALID) {
+            this_token->ttype = t;
+        }
+        return build_leaf();
+    } else {
+        if(this_token->ttype == TOK_LPAREN && (next_token->ttype == TOK_UMINUS 
+        || next_token->ttype == TOK_NOT)){
+            node_t *node = calloc(1, sizeof(node_t));
+            advance_lexer();
+            node->tok = this_token->ttype;
+            advance_lexer();
+            node->children[0] = build_exp();
+            node->node_type = NT_INTERNAL;
+            node->type = NO_TYPE;
+            advance_lexer();
+            return node;
+        } else if(this_token->ttype == TOK_LPAREN){
+            node_t *node = calloc(1, sizeof(node_t));
+            advance_lexer();
+            node->children[0] = build_exp();
+            advance_lexer();
+            if(this_token->ttype == TOK_PLUS || this_token->ttype == TOK_TIMES ||
+                this_token->ttype == TOK_DIV || this_token->ttype == TOK_MOD ||
+                this_token->ttype == TOK_EQ || this_token->ttype == TOK_LT ||
+                this_token->ttype == TOK_GT || this_token->ttype == TOK_AND || 
+                this_token->ttype == TOK_OR || this_token->ttype == TOK_QUESTION) {
+                node->node_type = NT_INTERNAL;
+                node->type = NO_TYPE;
+                node->tok = this_token->ttype;
+            }
+            advance_lexer();
+            node->children[1] = build_exp();
+            if(next_token->ttype == TOK_COLON){
+                advance_lexer();
+                advance_lexer();
+                node->children[2] = build_exp();
+            }
+            advance_lexer();
+            if(this_token->ttype == TOK_RPAREN){
+                return node;
+            } 
+        } 
+        return NULL;
+    }
+}
+
+/* build_root() - construct the root of the AST for the current input
+ * This function is provided to you. Use it as a reference for your code
+ * Parameter: none
+ * Return value: the root of the AST */
+static node_t *build_root(void) {
+    // check running status
+    if (terminate || ignore_input) return NULL;
+
+    // allocate memory for the root node
+    node_t *ret = calloc(1, sizeof(node_t));
+    if (! ret) {
+        // calloc returns NULL if memory allocation fails
+        logging(LOG_FATAL, "failed to allocate node");
+        return NULL;
+    }
+
+    // set the node struct's fields
+    ret->node_type = NT_ROOT;
+    ret->type = NO_TYPE;
+
+    // (EEL-2) check for variable assignment
+    if (this_token->ttype == TOK_ID && next_token->ttype == TOK_ASSIGN) {
+        if (check_reserved_ids(this_token->repr) != TOK_INVALID) {
+            logging(LOG_ERROR, "variable name is reserved");
+            return ret;
+        }
+        ret->type = ID_TYPE;
+        ret->children[0] = build_leaf();
+        advance_lexer();
+        advance_lexer();
+        ret->children[1] = build_exp();
+        if (next_token->ttype != TOK_EOL) {
+            handle_error(ERR_SYNTAX);
+        }
+        return ret;
+    }
+    
+    // build an expression based on the current token
+    // this will be where the majority of the tree is recursively constructed
+    ret->children[0] = build_exp();
+
+    // if the next token is End of Line, we're done
+    if (next_token->ttype == TOK_EOL)
+        return ret;
+    else {                                     
+    /* At this point, we've finished building the main expression. The only
+     * syntactically valid tokens that could remain would be format specifiers */    
+        
+        // check that our next token is a format specifier
+        if (next_token->ttype != TOK_SEP) {
+            handle_error(ERR_SYNTAX);
+            return ret;
+        }
+
+        advance_lexer();
+        
+        // check that there is an ID following the format specifier
+        if (next_token->ttype != TOK_ID) {
+            handle_error(ERR_SYNTAX);
+            return ret;
+        }
+
+        // check that the ID is a format specifier ID
+        if (id_is_fmt_spec(next_token->repr))
+            next_token->ttype = TOK_FMT_SPEC;
+        if (next_token->ttype != TOK_FMT_SPEC) {
+            handle_error(ERR_SYNTAX);
+            return ret;
+        }
+
+        advance_lexer();
+
+        // build the leaf for the format specifier. 
+        // if any tokens besides EOL remain, the syntax is not valid
+        ret->children[1] = build_leaf();
+        if (next_token->ttype != TOK_EOL) {
+            handle_error(ERR_SYNTAX);
+            return ret;
+        }
+        return ret;
+    }
+
+    // this return statement will only be reached if there was a syntax error
+    handle_error(ERR_SYNTAX);
+    return ret;
+} 
+
+/* read_and_parse - return the root of an AST representing the current input
+ * Parameter: none
+ * Return value: the root of the AST */
+node_t *read_and_parse(void) {
+    init_lexer();
+    return build_root();
+}
+
+/* cleanup() - given the root of an AST, free all associated memory
+ * Parameter: The root of an AST
+ * Return value: none
+ * (STUDENT TODO) */
+void cleanup(node_t *nptr) {
+    // Is it enough to free the node the function called upon?
+    if(nptr != NULL){
+        cleanup(nptr->children[0]);
+        cleanup(nptr->children[1]);
+        free(nptr);
+    }
+}
